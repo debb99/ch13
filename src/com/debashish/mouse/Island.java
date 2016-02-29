081557ab65b6a1d55583ca1979cb713999a9fa9c
Debashis Biswas <S17400233@stu.tuhsd.adn>
2016-02-29 11:32:13 -0700

    significant progress on Island prog

--- src/com/debashish/mouse/Island.java
+++ src/com/debashish/mouse/Island.java
@@ -3,25 +3,21 @@ package com.debashish.mouse;
 import java.util.Random;
 
 public class Island {
-	public enum Status{
-		SEARCHING,
-		ESCAPED,
-		DROWNED,
-		STARVED
+	public enum Status {
+		SEARCHING, ESCAPED, DROWNED, STARVED
 	}
-	
+
 	public Island() {
 		gen = new Random();
 	}
-	
-	public int[][] createIsland(){
-		//need a 2D array var over here, that's why this is confusing
+
+	public int[][] createIsland() {
 		int[][] array = new int[10][15];
-		
-		for(int i = 0; i < array.length; i++){
-			for(int j = 0; j < array[i].length; j++){
-				if(i == 0 || i == array.length - 1 || j == 0 || j == array[i].length - 1){
-					if(gen.nextInt(100) < PERCENT_BRIDGES){
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (i == 0 || i == array.length - 1 || j == 0 || j == array[i].length - 1) {
+					if (gen.nextInt(100) < PERCENT_BRIDGES) {
 						array[i][j] = -1;
 					} else {
 						array[i][j] = 0;
@@ -29,38 +25,95 @@ public class Island {
 				}
 			}
 		}
-		
+
 		x = array[0].length / 2;
 		y = array.length / 2;
-		
+
 		return array;
 	}
-	
-	public void printIsland(int[][] arr){
-		for(int i = 0; i < arr.length; i++){
-			for(int j = 0; j < arr[i].length; j++){
+
+	public void printIsland(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				System.out.printf("%6d", arr[i][j]);
 			}
 			System.out.println();
 		}
 	}
-	
-	public Status moveAbout(){
+
+	public int moveAbout() {
 		Status status = Status.SEARCHING;
-		for(int i = 0; i < 50; i++){
+		
+		int i = 0;
+		int numpass = 50;
+		
+		for (i = 0; i < numpass; i++) {
+			switch (gen.nextInt(4)) {
+			case 0:
+				y--;
+				break;
+			case 1:
+				x++;
+				break;
+			case 2:
+				y++;
+				break;
+			case 3:
+				x--;
+				break;
+			default:
+				break;
+			}
 			
+			if(y == 0 || y == Island.length - 1 || x == 0 || x == Island[y].length - 1){
+				switch(Island[y][x]){
+				case 0:
+					status = Status.ESCAPED;
+					break;
+				case -1:
+					status = Status.DROWNED;
+					break;
+				}
+				break;
+			}
+
+			Island[y][x] = i;
 		}
 		
-		return status;
-	}
-	
-	public void simulate(){
+		if(i >= numpass){
+			status = Status.STARVED;
+		}
 		
+		finalStatus = status;
+
+		return i;
 	}
-	
+
+	public Status simulate() {
+		Island = createIsland();
+		
+		int turn = moveAbout();
+		printIsland(Island);
+		
+		String indent = "\t\t\t\t";
+		
+		if(finalStatus == Status.ESCAPED){
+			System.out.println(indent + "The mouse has escaped on turn " + turn);
+		} else if(finalStatus == Status.DROWNED){
+			System.out.println(indent + "The mouse unfortunately drowned on turn " + turn);
+		} else if (finalStatus == Status.STARVED){
+			System.out.println(indent + "The mouse has run out of food, starving on turn " + turn);
+		}
+		
+		System.out.println("\n");
+		
+		return finalStatus;
+	}
+
+	Status finalStatus;
 	private int[][] Island;
 	private int y;
 	private int x;
 	private Random gen;
-	private final int PERCENT_BRIDGES = 50;
+	private final int PERCENT_BRIDGES = 30;
 }
--- src/com/debashish/mouse/MouseTest.java
+++ src/com/debashish/mouse/MouseTest.java
@@ -3,6 +3,31 @@ package com.debashish.mouse;
 public class MouseTest {
 	public static void main(String[] args) {
 		Island Game = new Island();
-		Game.simulate();
+		
+		int[] values = new int[4];
+		for(int a : values){
+			a = 0;
+		}
+		
+		//1 = ESC
+		//2 = DROWN
+		//3 = STARVED
+		
+		for(int i = 0; i < 1000; i++){
+			Island.Status tempStatus = Game.simulate();
+			if(tempStatus == Island.Status.ESCAPED){
+				values[1]++;
+			}
+			
+			if(tempStatus == Island.Status.DROWNED){
+				values[2]++;
+			}
+
+			if(tempStatus == Island.Status.STARVED){
+				values[3]++;
+			}
+		}
+		
+		System.out.printf("Escaped: %d%nDrowned: %d%nStarved: %d%n", values[1], values[2], values[3]);
 	}
 }
